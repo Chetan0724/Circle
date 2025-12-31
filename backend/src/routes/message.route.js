04a06c0eb47fb899eb67ea0ca324a325dfdd6690
Chetan Yadav <ydvchetan01@gmail.com>
2025-12-31 15:50:01 +0530

    Implement messaging functionality

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const router = Router();
 
-router.get("/send", (req, res) => {
-  res.send("Message Sent!");
-});
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
