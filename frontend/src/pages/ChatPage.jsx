5b875c1faf775d1f9925de9110380ecbd07053f9
Chetan Yadav <ydvchetan01@gmail.com>
2026-01-01 15:05:52 +0530

    frontend login page

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,13 @@
-const ChatPage = () => {
-  return (
-    <div>ChatPage</div>
-  )
-}
+import { useAuthStore } from "../store/useAuthStore";
 
-export default ChatPage
+const ChatPage = () => {
+  const { logout } = useAuthStore();
+  return (
+    <div className="z-10">
+      ChatPage
+      <button onClick={logout}>logout</button>
+    </div>
+  );
+};
+
+export default ChatPage;
