534c37e8b385afb8cda983f3c86a865a1447dab0
Chetan Yadav <ydvchetan01@gmail.com>
2026-01-01 14:33:59 +0530

    Add CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,15 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
